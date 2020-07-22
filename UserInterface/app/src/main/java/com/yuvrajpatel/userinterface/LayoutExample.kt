542d1f9e8ef1683a6ccc702c32c22e7761df94a2
Yuvraj Patel <yuvipatel003@gmail.com>
2020-07-22 13:26:57 -0400

    Update LayoutExample.kt

--- UserInterface/app/src/main/java/com/yuvrajpatel/userinterface/LayoutExample.kt
+++ UserInterface/app/src/main/java/com/yuvrajpatel/userinterface/LayoutExample.kt
@@ -1,5 +1,7 @@
 package com.yuvrajpatel.userinterface
 
+import android.accessibilityservice.GestureDescription
+import android.content.DialogInterface
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
@@ -7,6 +9,8 @@ import android.text.TextUtils
 import android.view.View
 import android.widget.Button
 import android.widget.TextView
+import androidx.appcompat.app.AlertDialog
+import java.nio.file.attribute.AclEntry
 
 class LayoutExample : AppCompatActivity() {
 
@@ -21,8 +25,23 @@ class LayoutExample : AppCompatActivity() {
         mBtnClickMe = findViewById(R.id.btn_ClickMe)
 
         mBtnClickMe.setOnClickListener(View.OnClickListener {
-            val intent = Intent(this, Registration::class.java)
-            startActivity(intent)
+
+            val alertDialog = AlertDialog.Builder(this)
+            alertDialog.setTitle("Are you sure !")
+            alertDialog.setMessage("Do you want to redirect on Registration?")
+            alertDialog.setPositiveButton("Yes",{dialogInterface : DialogInterface, i: Int ->
+                val intent = Intent(this, Registration::class.java)
+                startActivity(intent)
+                finish()
+            })
+
+            alertDialog.setNegativeButton("No", { dialogInterface: DialogInterface, i: Int ->
+                dialogInterface.dismiss()
+            })
+
+            alertDialog.setNeutralButton("Any",{ dialogInterface: DialogInterface, i: Int -> })
+            alertDialog.show()
+
         })
 
         mBtnClickMe.setOnLongClickListener(View.OnLongClickListener {
